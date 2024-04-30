de2bb7a19ab583e9eb79f0ac08b72738255b26db
dayvidpham <dayvidpham@gmail.com>
2024-04-29 23:47:06 -0700

    Add generated Spring Boot code

---
+++ src/test/java/com/dhpham/spotify/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhpham.spotify.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
