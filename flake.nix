dbb3ede582bf564947b2762b6155cf2d3e918830
dayvidpham <dayvidpham@gmail.com>
2024-05-01 02:43:45 -0700

    Reformats according to nixpkgs-fmt

--- flake.nix
+++ flake.nix
@@ -1,77 +1,73 @@
 {
-    inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-        flake-utils.url = "github:numtide/flake-utils";
-    };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-    outputs = inputs@{ self, nixpkgs, flake-utils, ... }: 
+  outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
-    let
+      let
         pkgs = nixpkgs.legacyPackages.${system};
         java = pkgs.jdk21_headless;
-        idea =  pkgs.jetbrains.idea-community;
+        idea = pkgs.jetbrains.idea-community;
         # idea = pkgs.callPackage pkgs.jetbrains.idea-community { jdk = java; };
-        devNativeBuildInputs = [
-          idea
-          java
-          pkgs.maven
-        ];
+        devNativeBuildInputs = [ idea java pkgs.maven ];
 
         postgres = pkgs.postgresql_16_jit;
-        postgresNativeBuildInputs = [
-            postgres
-        ];
+        postgresNativeBuildInputs = [ postgres ];
 
         env = {
-            PGDATA = "./pg";
-            PGHOST = "localhost";
-            PGPORT = "5555";
+          PGDATA = "./pg";
+          PGHOST = "localhost";
+          PGPORT = "5555";
         };
-    in {
+      in
+      {
         devShells.default = pkgs.mkShell {
-            inherit env;
-            nativeBuildInputs = devNativeBuildInputs;
+          inherit env;
+          nativeBuildInputs = devNativeBuildInputs;
         };
 
         devShells.postgres = pkgs.mkShell {
-            inherit env;
+          inherit env;
+          nativeBuildInputs = postgresNativeBuildInputs;
 
-            nativeBuildInputs = postgresNativeBuildInputs;
-            postgresConf =
-                pkgs.writeText "postgresql.conf" ''
-                    # Add Custom Settings
-                    log_min_messages = warning
-                    log_min_error_statement = error
-                    log_min_duration_statement = 100  # ms
-                    log_connections = on
-                    log_disconnections = on
-                    log_duration = on
-                    #log_line_prefix = '[] '
-                    log_timezone = 'UTC'
-                    log_statement = 'all'
-                    log_directory = 'pg_log'
-                    log_filename = 'postgresql-%Y-%m-%d_%H-%M-%S.log'
-                    logging_collector = on
-                    log_min_error_statement = error
-                '';
+          postgresConf = pkgs.writeText "postgresql.conf" ''
+            # Add Custom Settings
+            log_min_messages = warning
+            log_min_error_statement = error
+            log_min_duration_statement = 100  # ms
+            log_connections = on
+            log_disconnections = on
+            log_duration = on
+            #log_line_prefix = '[] '
+            log_timezone = 'UTC'
+            log_statement = 'all'
+            log_directory = 'pg_log'
+            log_filename = 'postgresql-%Y-%m-%d_%H-%M-%S.log'
+            logging_collector = on
+            log_min_error_statement = error
+          '';
 
-            # Post Shell Hook
-            shellHook = ''
-                echo "Using ${postgres.name}."
+          # Post Shell Hook
+          shellHook = ''
+            echo "Using ${postgres.name}."
 
-                # Setup: DB
-                if [ ! -d $PGDATA ]
-                then
-                    pg_ctl initdb -o "-U postgres"
-                    cat "$postgresConf" >> $PGDATA/postgresql.conf
-                fi
+            # Setup: DB
+            if [ ! -d $PGDATA ]
+            then
+                pg_ctl initdb -o "-U postgres"
+                cat "$postgresConf" >> $PGDATA/postgresql.conf
+            fi
 
-                pg_ctl -o "-k ./" start
+            pg_ctl -o "-k ./" start
 
-                # Convenience aliases
-                alias fin="pg_ctl stop && exit"
-                alias pg="psql -p 5555 -U postgres"
-            '';
+            # Convenience aliases
+            alias fin="pg_ctl stop && exit"
+            alias pg="psql -p 5555 -U postgres"
+          '';
         };
-    });
+
+        formatter = pkgs.nixpkgs-fmt;
+      });
 }
